787f3f4aceb1b82a596129e178c4256612e24a23
baaij3201 <bajia2013@163.com>
2025-02-06 17:00:31 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,6 +1,18 @@
 #ifndef geme
 #define game
-#include<bits/stdc++.h>
+#define kd(VK_NONAME) ((GetAsyncKeyState(VK_NONAME)&0x8000)?1:0)
+#define dn(VK_NONAME) keybd_event(VK_NONAME,0,0,0)
+#define up(VK_NONAME) keybd_event(VK_NONAME,0,0,0)
+#define ps(VK_NONAME) up(VK_NONAME),dn(VK_NONAME)
+#define mgb(a,b,c) MessageBox(NULL,a,b,c)
+//#include"stdc++.h"
 #include<windows.h>
+#include<algorithm>
+#include<iostream>
+#include<cstring>
+#include<iomanip>
 #include<conio.h>
+#include<fstream>
+//#include<easyx.h>
+#include<time.h>
 #endif
