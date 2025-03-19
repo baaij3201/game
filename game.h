4255d666af9b9c22d38ef09f0860b08f81e6b8bc
baaij3201 <bajia2013@163.com>
2025-03-19 22:00:01 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,10 +1,5 @@
 #ifndef geme
 #define game
-#define kd(VK_NONAME) ((GetAsyncKeyState(VK_NONAME)&0x8000)?1:0)
-#define dn(VK_NONAME) keybd_event(VK_NONAME,0,0,0)
-#define up(VK_NONAME) keybd_event(VK_NONAME,0,0,0)
-#define ps(VK_NONAME) up(VK_NONAME),dn(VK_NONAME)
-#define mgb(a,b,c) MessageBox(NULL,_T(a),_T(b),c)
 //#include"stdc++.h"
 #include<windows.h>
 #include<algorithm>
@@ -13,6 +8,26 @@
 #include<iomanip>
 #include<conio.h>
 #include<fstream>
+#include<tchar.h>
 //#include<easyx.h>
 #include<time.h>
+//#define randmax() (rand()<<15|rand())
+//#define randpro(a,b) ((rand()%(b-a+1))+a)
+namespace randtools {
+	int randmax() { return (rand() << 15) | rand(); }
+	int randpro(int a, int b) { return (rand() % (b - a + 1)) + a; }
+};
+#define kd(VK_NONAME) ((GetAsyncKeyState(VK_NONAME)&0x8000)?1:0)
+#define dn(VK_NONAME) keybd_event(VK_NONAME,0,0,0)
+#define up(VK_NONAME) keybd_event(VK_NONAME,0,0,0)
+#define ps(VK_NONAME) up(VK_NONAME),dn(VK_NONAME)
+int mgb(const char* a, const char* b, unsigned int c) {
+	return MessageBox(NULL, _T(a), _T(b), c);
+}
+namespace SBtools {
+	bool SB;
+	void editsb(bool t) {
+		SB = t;
+	}
+};
 #endif
